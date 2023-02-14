1b4e9e8455bc97b13a4cf1f5664b09506058ee2c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-14 09:51:21 +0800

    Update loops.py

--- fundamentals/write-logic-with-python/loops.py
+++ fundamentals/write-logic-with-python/loops.py
@@ -2,3 +2,12 @@
 #
 #
 
+# For the following, we wanted to calculate the average rating of an app. 
+# We currently have 7,197 rows unlike before when we only had 5 rows.
+# To do this task, we do the following:
+
+# 1. Retrieve each individual rating
+# 2. Sum the ratings
+# 3. Divide by the number of ratings.
+#
+
