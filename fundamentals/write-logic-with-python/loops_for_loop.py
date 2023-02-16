735ec396a23cd3b17f2daee9d379ff3169c76cd8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-16 10:45:19 +0800

    Update loops_for_loop.py

--- fundamentals/write-logic-with-python/loops_for_loop.py
+++ fundamentals/write-logic-with-python/loops_for_loop.py
@@ -118,6 +118,16 @@ But using the for row in app_data_set technique requires us to write only two li
 — the dataset can have five rows or one million!
 '''
 
+row_1 = ['Facebook', 0.0, 'USD', 2974676, 3.5]
+row_2 = ['Instagram', 0.0, 'USD', 2161558, 4.5]
+row_3 = ['Clash of Clans', 0.0, 'USD', 2130805, 4.5]
+row_4 = ['Fruit Ninja Classic', 1.99, 'USD', 698516, 4.5]
+row_5 = ['Minecraft: Pocket Edition', 6.99, 'USD', 522012, 4.5]
+
+app_data_set = [row_1, row_2, row_3, row_4, row_5]
+
+for rating_count in app_data_set:
+    print(rating_count[3])
 
 
 
