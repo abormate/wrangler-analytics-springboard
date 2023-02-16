c9530de33e02c9e30515460103a85c3d32779a8c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-16 10:49:03 +0800

    Update loop_structure.py

--- fundamentals/write-logic-with-python/loop_structure.py
+++ fundamentals/write-logic-with-python/loop_structure.py
@@ -5,3 +5,13 @@
 Now that we know how to use for loops to print list elements, let's learn how to use for loops to sum values in a list and store the result. 
 To store the results from iterating over a list, we need to learn more about for loop structure. These are the structural parts of a for loop:
 '''
+
+'''
+The indented code in the body gets executed the same number of times as elements in the iterable variable. If the iterable variable is a list containing three elements, the indented code in the body gets executed three times. We call each code execution an iteration, so there will be three iterations for a list that has three elements. For each iteration, the iteration variable will take a different value, following this pattern:
+
+For the first iteration, the value is the first element of the iterable (if the iterable is the list [1, 3, 5], then the value will be 1).
+
+For the second iteration, the value is the second element of the iterable (if the iterable is the list [1, 3, 5], then the value will be 3).
+
+For the third iteration, the value is the third element of the iterable (if the iterable is the list [1, 3, 5], then the value will be 5).
+''''
