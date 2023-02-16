6036dab381cc43ec05f9d51b4a9ae9cb5457b768
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-16 09:22:01 +0800

    Update loops_for_loop.py

--- fundamentals/write-logic-with-python/loops_for_loop.py
+++ fundamentals/write-logic-with-python/loops_for_loop.py
@@ -69,7 +69,17 @@ print(avg_rating)
 #
 # The same could be accomplished as on Code a3 -- with for loops 
 
+row_1 = ['Facebook', 0.0, 'USD', 2974676, 3.5]
+row_2 = ['Instagram', 0.0, 'USD', 2161558, 4.5]
+row_3 = ['Clash of Clans', 0.0, 'USD', 2130805, 4.5]
+row_4 = ['Fruit Ninja Classic', 1.99, 'USD', 698516, 4.5]
+row_5 = ['Minecraft: Pocket Edition', 6.99, 'USD', 522012, 4.5]
 
+app_data_set = [row_1, row_2, row_3, row_4, row_5]
+
+for row in app_data_set:
+    rating = row[-1]
+    print(rating)
 
 
 
