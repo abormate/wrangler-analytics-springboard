34e1d9ffd0fd3003405f5fdea232603e008bd388
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-17 12:12:25 +0800

    Update find-average-value-for-loop.py

--- fundamentals/write-logic-with-python/find-average-value-for-loop.py
+++ fundamentals/write-logic-with-python/find-average-value-for-loop.py
@@ -22,3 +22,26 @@ avg_rating = (app_data_set[0][-1] + app_data_set[1][-1] +
               app_data_set[4][-1]) / 5
 
 print(avg_rating)
+
+#
+#
+# Doing the above with a loop, instead
+#
+
+
+row_1 = ['Facebook', 0.0, 'USD', 2974676, 3.5]
+row_2 = ['Instagram', 0.0, 'USD', 2161558, 4.5]
+row_3 = ['Clash of Clans', 0.0, 'USD', 2130805, 4.5]
+row_4 = ['Fruit Ninja Classic', 1.99, 'USD', 698516, 4.5]
+row_5 = ['Minecraft: Pocket Edition', 6.99, 'USD', 522012, 4.5]
+
+app_data_set = [row_1, row_2, row_3, row_4, row_5]
+
+rating_sum = 0
+for element in app_data_set:
+    rating = element[-1]
+    rating_sum += rating 
+    
+avg_rating = rating_sum / len(app_data_set) 
+
+print(avg_rating)
