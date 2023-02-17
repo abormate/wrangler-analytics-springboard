407141ef399a839b0af309409b54d58d0a5c2738
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-17 11:13:38 +0800

    Update and rename loop_structure.py to for_loop_structure.py

--- fundamentals/write-logic-with-python/for_loop_structure.py
+++ fundamentals/write-logic-with-python/for_loop_structure.py
@@ -65,5 +65,37 @@ print(sum)
 # 4
 # 9
 # 9
+#
+
+# Practice a03
+#
+'''
+Calculate the total number of app ratings for the apps stored in the app_data_set variable.
+
+1. Initialize a variable named rating_sum with a value of zero outside the loop body.
+
+2. Loop (iterate) over the app_data_set list of lists. For each of the five iterations of the loop (for each row in app_data_set):
+-- -- Extract the rating count of the app, and store it to a variable named rating_count.
+-- -- Add the value stored in rating_count to the current value of rating_sum and assign the result back to rating_sum.
+-- -- Print rating_sum.
+
+'''
+
+# actual code
+
+row_1 = ['Facebook', 0.0, 'USD', 2974676, 3.5]
+row_2 = ['Instagram', 0.0, 'USD', 2161558, 4.5]
+row_3 = ['Clash of Clans', 0.0, 'USD', 2130805, 4.5]
+row_4 = ['Fruit Ninja Classic', 1.99, 'USD', 698516, 4.5]
+row_5 = ['Minecraft: Pocket Edition', 6.99, 'USD', 522012, 4.5]
+
+app_data_set = [row_1, row_2, row_3, row_4, row_5]
+
+rating_sum = 0
+for element in app_data_set:
+    rating_count = element[-2]
+    rating_sum += rating_count
+    print(rating_sum)
+
 
 
