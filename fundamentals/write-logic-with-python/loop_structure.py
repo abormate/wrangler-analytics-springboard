be3b2580f82ae8c9cae631a8787bf9dc6bb2c707
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-17 09:47:22 +0800

    Update loop_structure.py

--- fundamentals/write-logic-with-python/loop_structure.py
+++ fundamentals/write-logic-with-python/loop_structure.py
@@ -1,11 +1,6 @@
 # For Loop structure
 #
 #
-#
-#
-#
-#
-#
 '''
 Now that we know how to use for loops to print list elements, let's learn how to use for loops to sum values in a list and store the result. 
 To store the results from iterating over a list, we need to learn more about for loop structure. These are the structural parts of a for loop:
