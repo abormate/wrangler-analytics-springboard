6943f6c4ee0bc6442cc0500e07a813cbf58108d2
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-17 12:07:15 +0800

    Update and rename find-average-for-loop.py to find-average-value-for-loop.py

--- fundamentals/write-logic-with-python/find-average-for-loop.py
+++
@@ -1,9 +0,0 @@
-# Learn
-# Recall last time our method of calculating the average app rating
-#
-'''
-1. Retrieve each app rating
-2. Sum the ratings
-3. Divide by the number of ratings for the apps
-
-'''
