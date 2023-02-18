c6f8fe34fc0844f6e6411fe8a8ce522b0074011e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-18 14:05:59 +0800

    Create working-with-files.py

---
+++ fundamentals/write-logic-with-python/working-with-files.py
@@ -0,0 +1,10 @@
+# Learn
+# 
+# automate data ingestion
+'''
+We could type each data point and bundle them efficiently into a list of lists. The dataset above, however, has 7,197 rows and 16 columns, 
+which amounts to 115,152 (7,197 × 16) data points — typing all that would take us days. We'd also be bound to make typing errors, 
+which could lead to wrong data and false conclusions. Fortunately, we can leverage Python to store this dataset as a list of lists 
+in a matter of seconds.
+'''
+
