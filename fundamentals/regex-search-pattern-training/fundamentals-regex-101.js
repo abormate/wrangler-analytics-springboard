87c60c181d61af4841c6e7f0801368a7453e3ce6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-23 09:33:57 +0800

    Lesson 22 for regular expressions

--- fundamentals/regex-search-pattern-training/fundamentals-regex-101.js
+++ fundamentals/regex-search-pattern-training/fundamentals-regex-101.js
@@ -1,8 +1,8 @@
-// ---------------------- // 
+// ------------------------------------------- // 
 // 
 // Regular Expressions -- 101 training
 //
-// ---------------------- //
+// ------------------------------------------- //
 
 // Curly braces -- lesson 21
 /*
@@ -20,9 +20,24 @@ ber beer beeer beeeer
 
 // actual regex, following the briefing or instructions above
 
-// be{2}r 
+// ------>>> //     be{2}r 
 
-// ------------------------- //
+// ------------------------------------------- //
+
+// Curly braces - part 2 - lesson 22
+/*
+To express at least a certain number of occurrences of a character, immediately after the character 
+we write at least how many times we want it to occur followed by a comma , and wrapped inside curly 
+braces {n, }. For example, indicate that the following letter e can occur at least 3 times.
+*/
+
+// sample text -- tester
+// ber beer beeer beeeer
+
+// actual regex
+// ------>>>> //     be{3,}r
+
+// ----------------------------------------- // 
 
 
 
