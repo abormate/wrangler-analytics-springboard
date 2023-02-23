b24019ef4293270f78e42a299de1785caf06fbc6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-23 09:27:30 +0800

    Added regex training 101

---
+++ fundamentals/regex-search-pattern-training/fundamentals-regex-101.js
@@ -0,0 +1,29 @@
+// ---------------------- // 
+// 
+// Regular Expressions -- 101 training
+//
+// ---------------------- //
+
+// Curly braces -- lesson 21
+/*
+instructions:
+
+To express a certain number of occurrences of a character, at the end we write curly braces 
+{n} along with how many times we want it to occur. For example, indicate that the following 
+letter e can occur only 2 times.
+*/
+
+// text sample
+/*
+ber beer beeer beeeer
+*/
+
+// actual regex, following the briefing or instructions above
+
+// be{2}r 
+
+// ------------------------- //
+
+
+
+
