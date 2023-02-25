7e87f9ab1aba331d6964f58d337e8b73b8a35c84
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-25 08:45:51 +0800

    Lesson 51 - Regex 101 - Modifiers

--- fundamentals/regular-expressions-search-pattern-training/regex-101-modifiers.js
+++ fundamentals/regular-expressions-search-pattern-training/regex-101-modifiers.js
@@ -5,4 +5,22 @@ Flags determine whether the typed expression treats text as separate lines, is c
 or finds all matches. Continue to the next step to learn the flags.
 */
 
+// ----------------------------------------------------------------- //
+
 // Lesson 51 -- global flag
+/*
+The global flag causes the expression to select all matches. If not used it will only select the 
+first match. Now enable the global flag to be able to select all matches.
+*/
+
+// sample line of text to evaluate our regex input on
+// domain.com, test.com, site.com
+
+// actual regex input -- it passes when we enable "global" -- ctrl+g
+// ------->> //     \w+\.com 
+
+// END -- Lesson 51
+
+// ----------------------------------------------------------------- //
+
+
