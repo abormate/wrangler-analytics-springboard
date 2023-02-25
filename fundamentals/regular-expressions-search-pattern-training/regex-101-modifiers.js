8de47bc9ba14e512a7c98225a52f0da90ad20bdb
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-25 08:41:04 +0800

    Add: Modifiers lesson module for regex 101

---
+++ fundamentals/regular-expressions-search-pattern-training/regex-101-modifiers.js
@@ -0,0 +1,8 @@
+/*
+"Flags" -- or Modifiers
+Flags change the output of the expression. That's why flags are also called modifiers. 
+Flags determine whether the typed expression treats text as separate lines, is case sensitive, 
+or finds all matches. Continue to the next step to learn the flags.
+*/
+
+// Lesson 51 -- global flag
