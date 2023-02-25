c9f96a494ed0aedcb2065ea321dadef44d52af55
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-25 14:21:08 +0800

    Programming for Analytics - Python - 2/25/2023

--- fundamentals/write-logic-with-python/working-with-errors.py
+++ fundamentals/write-logic-with-python/working-with-errors.py
@@ -163,6 +163,14 @@ from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
+rating_sum = 0
+
+for row in apps_data[1:]:
+    rating = float(row[7])
+    rating_sum += rating
+
+avg_rating = rating_sum/len(apps_data[1:])
+print(avg_rating)
 
 
 
