7ed7373e37a088d875b4bbbe8d205e32a4cba1ea
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-25 14:29:12 +0800

    Programming for Analytics - Python - Feb 25, 2023

---
+++ fundamentals/python-programming-for-analytics-learnspace/alternative-method-calc-average-value.py
@@ -0,0 +1,32 @@
+# --------------------------------- #
+#
+# Calculate the average value from a list of lists
+#
+# --------------------------------- #
+
+# Learn
+"""
+Now we'll learn an alternative method for calculating the average rating value. Once we create a list, 
+we can add (or append) values to it using the append() function. Let's take a look:
+"""
+
+# Example code
+a_list = [1, 2]
+a_list.append(3)
+print(a_list)
+
+# Output
+[1, 2, 3]
+
+# New knowledge
+# 
+# Here's how to append values to an empty list:
+empty_list = []
+empty_list.append(12)
+print(empty_list)
+
+# Output
+[12]
+
+
+
