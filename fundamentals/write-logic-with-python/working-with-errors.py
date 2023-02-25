148b44e2a4981945646f897bcad5cd95f04bf7f6
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-25 14:09:48 +0800

    Training: Write analytics logic with Python - 2/25/2023

--- fundamentals/write-logic-with-python/working-with-errors.py
+++ fundamentals/write-logic-with-python/working-with-errors.py
@@ -123,6 +123,17 @@ included for the first error we saw above.
 """
 
 
+from csv import reader
+
+opened_file = open('AppleStore.csv')
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+rating_sum = 0
+for row in apps_data[1:]:
+    rating = float(row[7])
+    rating_sum = rating_sum + rating
+print(rating_sum)
 
 
 
