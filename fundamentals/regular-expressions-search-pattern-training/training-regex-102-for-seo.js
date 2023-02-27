571f700b439296916b74b08d9fbfe2fdec59ada8
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-27 08:07:58 +0800

    Training - Regex 102 - for Online Marketing Analytics

---
+++ fundamentals/regular-expressions-search-pattern-training/training-regex-102-for-seo.js
@@ -0,0 +1,12 @@
+// ------------------------------------- //
+//
+// Regex 102 -- for Organic Online Marketing
+//
+// ------------------------------------- //
+
+/*
+Regex is a set of regular expressions frequently used in many fields, especially by people who want 
+to increase their productivity. The advantages of using Regex for SEO are that it makes it easier 
+to analyze big data and write richer filters.
+*/
+
