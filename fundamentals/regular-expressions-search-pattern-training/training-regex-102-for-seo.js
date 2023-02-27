f973cc7998a6537173a9cd6c275262f482988d70
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-27 08:15:01 +0800

    Lesson 2 - Regex 102 - Use Cases

--- fundamentals/regular-expressions-search-pattern-training/training-regex-102-for-seo.js
+++ fundamentals/regular-expressions-search-pattern-training/training-regex-102-for-seo.js
@@ -10,3 +10,32 @@ to increase their productivity. The advantages of using Regex for SEO are that i
 to analyze big data and write richer filters.
 */
 
+
+// ------------------------------------- //
+
+// Lesson 2
+// Use Cases
+/*
+You can use Regex for SEO on platforms such as Google Analytics, Google Data Studio, Google Sheets, 
+Google Search Console, Ahrefs, Deepcrawl, Screaming Frog. It is also frequently used in .htaccess 
+and robots.txt (regex-like syntax) files.
+*/
+
+
+// Sample lines of text to apply input regex with
+/*
+Google Analytics
+Google Data Studio
+Google Sheets
+Google Search Console
+Ahrefs
+*/
+
+// actual regex input 
+// ---------->> //      ^Google
+
+// END -- Lesson 2
+
+// ------------------------------------- //
+
+// Lesson 3  
