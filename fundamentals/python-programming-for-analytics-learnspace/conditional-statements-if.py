4aa45c035034743e11f0fa8688b15a64d3241b60
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-28 13:01:27 +0800

    Training: CodeWorks - Python 101 - Analytics - Conditionals

--- fundamentals/python-programming-for-analytics-learnspace/conditional-statements-if.py
+++ fundamentals/python-programming-for-analytics-learnspace/conditional-statements-if.py
@@ -11,3 +11,11 @@
 To isolate only the ratings of the free apps, we need to add a condition to our code. Specifically, 
 we want to add a rating to the ratings list only if the price equals 0.0:
 '''
+
+# Follow link: Image screenshot from learning module --->> https://prnt.sc/tEtesNX9ruP2
+
+'''
+To implement the condition above (If the price equals 0.0, then do:) in our code, 
+we can use an if statement:
+'''
+
