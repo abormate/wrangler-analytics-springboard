b848d3ce8da3b9a8a5317d7dcbbe37a5d1bf9aad
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-28 13:05:23 +0800

    Training: CodeWorks - Python 101 - Analytics - If Conditionals

--- fundamentals/python-programming-for-analytics-learnspace/conditional-statements-if.py
+++ fundamentals/python-programming-for-analytics-learnspace/conditional-statements-if.py
@@ -24,3 +24,15 @@ we can use an if statement:
 # In the example above, we iterate over the apps_data[1:], and for each iteration, 
 # complete the following:
 
+'''
+-- -- Assign the rating as a float to a variable named rating.
+
+-- -- Assign the price as a float to a variable named price. (The price also comes as a string, 
+so we need to convert it to a float.)
+
+-- -- If the price equals 0.0, we append the rating to the ratings list (if the price is 0.0, 
+then it means the app must be free). Whenever price doesn't equal 0.0, the code ratings.append(rating) 
+doesn't execute.
+'''
+
+# Here are a few things to notice about the if statement:
