774beb2657e337121bcedbba3bf6657b0cfec371
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-28 12:59:35 +0800

    Training: CodeWorks - Python 101 - Analytics - Conditionals

---
+++ fundamentals/python-programming-for-analytics-learnspace/conditional-statements-if.py
@@ -0,0 +1,13 @@
+# ----------------------- #
+#
+# "If" Conditional Statements
+#
+# ----------------------- #
+
+# Learn 
+# ------------------------# 
+
+'''
+To isolate only the ratings of the free apps, we need to add a condition to our code. Specifically, 
+we want to add a rating to the ratings list only if the price equals 0.0:
+'''
