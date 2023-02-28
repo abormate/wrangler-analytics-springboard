4e321c5ef1aa5cea564a7c6b7230cff628a62166
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-02-28 12:41:22 +0800

    Training: Programming with Python for Analytics

--- fundamentals/python-programming-for-analytics-learnspace/recap-loops-101.py
+++ fundamentals/python-programming-for-analytics-learnspace/recap-loops-101.py
@@ -1,3 +1,19 @@
+# Learn 
+
+"""
+In this lesson on conditional statements, we'll learn how to ask more advanced questions about datasets, by using the following tools:
+
+How to use if statements
+How to use Boolean values
+How to use comparison operators with strings or lists
+
+Let's work with a dataset that stores information for 7,197 mobile apps and we'll ask questions like the following (in regard to average ratings):
+
+What's the average rating of non-free apps?
+What's the average rating of free apps?
+"""
+
+
 opened_file = open('AppleStore.csv')
 from csv import reader
 read_file = reader(opened_file)
