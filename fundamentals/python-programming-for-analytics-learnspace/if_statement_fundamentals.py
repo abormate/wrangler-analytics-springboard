845d9b98c306678510337fbdd3a9b97776dc747e
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-01 10:07:47 +0800

    Training - CodeWorks - Python for Analytics - If Statements

--- fundamentals/python-programming-for-analytics-learnspace/if_statement_fundamentals.py
+++ fundamentals/python-programming-for-analytics-learnspace/if_statement_fundamentals.py
@@ -12,3 +12,39 @@ always follow if: (1) a Boolean value, or (2) an expression that evaluates to a 
 Below, a boolean value follows if
 """
 
+if True:
+    print(100)
+
+# Output
+100
+
+
+# In the following code, 1 == 1 evaluates to True
+# -------------------------------------------- #
+
+if 1 == 1:
+    print(100)
+
+print(1 == 1)
+
+# Output
+100
+True
+
+"""
+We should indent the code in the body of an if statement four spaces to the right (technically, 
+we only need to indent the code one space to the right, but the convention in the Python community 
+is to use four spaces per indentation level — this is an important habit to form in case you collaborate 
+with others).
+"""
+
+if True:
+    print(1) # body
+
+# Output
+1
+
+# -------------------- #
+
+
+
