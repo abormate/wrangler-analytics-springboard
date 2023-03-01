593e6e6bbab1700e48d664fcdb5b37f708047d5d
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-01 09:45:04 +0800

    Training: CodeWorks - Python for Analytics - If Statements

---
+++ fundamentals/python-programming-for-analytics-learnspace/if_statement_fundamentals.py
@@ -0,0 +1,14 @@
+# -------------------------------------------- #
+#
+# If statements - foundational
+#
+# -------------------------------------------- #
+
+# Learn 
+
+"""
+Boolean values (True and False) are necessary parts of any if statement. One of the following must 
+always follow if: (1) a Boolean value, or (2) an expression that evaluates to a Boolean value. 
+Below, a boolean value follows if
+"""
+
