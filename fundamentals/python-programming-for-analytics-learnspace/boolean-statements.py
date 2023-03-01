4805f59bf8159f3257f5ce18d86f2e56e04d3915
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-01 08:10:46 +0800

    Training: CodeWorks - Python for Analytics - Booleans

---
+++ fundamentals/python-programming-for-analytics-learnspace/boolean-statements.py
@@ -0,0 +1,22 @@
+# --------------------------------- #
+#
+# Boolean statements 
+#
+# --------------------------------- #
+
+# Module learning
+#
+
+"""
+Previously, we used if price == 0.0 to check whether price equals 0.0. When we use the == operator 
+to determine if two values are equal, the output will always be True or False
+"""
+
+# Example
+print(4 == 4) # This is true
+print(4 == 7) # This is false
+
+# Output
+True
+False
+
