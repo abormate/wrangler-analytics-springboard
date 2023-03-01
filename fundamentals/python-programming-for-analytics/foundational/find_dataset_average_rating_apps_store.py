ec67e358afb122efd59f485c22cae12fd4c79617
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-01 11:12:40 +0800

    Training: CodeWorks - Python for Analytics - Find average rating for apps in AppStore

--- fundamentals/python-programming-for-analytics/foundational/find_dataset_average_rating_apps_store.py
+++ fundamentals/python-programming-for-analytics/foundational/find_dataset_average_rating_apps_store.py
@@ -112,12 +112,17 @@ from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
-free_apps_ratings = []
+non_free_apps_ratings = []
 for row in apps_data[1:]:
     rating = float(row[7])
     price = float(row[4])   
-    if price == 0.0:
-        free_apps_ratings.append(rating)
+    if price != 0.0:
+        non_free_apps_ratings.append(rating)
     
-avg_rating_free = sum(free_apps_ratings) / len(free_apps_ratings)
+avg_rating_non_free = sum(non_free_apps_ratings) / len(non_free_apps_ratings)
+print(avg_rating_non_free)
 
+# Output 
+3.720948742438714
+
+# -------------- END --------------- #
