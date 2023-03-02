bcafdf360eea981ffe8067f3a421c9388b5e8591
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-02 08:18:18 +0800

    Training - CodeWriting - Python for Analytics - If Else Statements

---
+++ fundamentals/python-programming-for-analytics/introduction-and-if-else-statements/find-average-rating-gaming-apps.py
@@ -0,0 +1,29 @@
+# ------------------------------------- #
+#
+# Find average of gaming apps in App Store
+#
+# ------------------------------------- #
+
+# Learn
+# --------------- #
+
+"""
+So far, we've used the == and != operators only with integers and floats. We can also use them with 
+other data types, such as strings or lists
+"""
+
+print('Games' == 'Music')
+print('Games' != 'Music')
+
+# Output 
+False
+True
+
+# --------------- #
+
+print([1,2,3] == [1,2,3])
+print([1,2,3] == [1,2,3,4])
+
+# Output
+True
+False
