197959c23040a021fc0a4906e482932023d6cb9c
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-02 08:22:31 +0800

    Training: CodeWriting - Python for Analytics - If and Else statements

--- fundamentals/python-programming-for-analytics/introduction-and-if-else-statements/find-average-rating-gaming-apps.py
+++ fundamentals/python-programming-for-analytics/introduction-and-if-else-statements/find-average-rating-gaming-apps.py
@@ -27,3 +27,17 @@ print([1,2,3] == [1,2,3,4])
 # Output
 True
 False
+
+# ---------------- #
+
+"""
+This enables us to answer more questions about our dataset, like the following:
+
+-- What's the average rating for gaming apps?
+-- What's the average rating for non-gaming apps?
+"""
+
+# Note that the prime_genre column describes the app genre, and the genre of gaming apps is encoded as 'Games'
+
+# image screencapture for "apps genre" --> https://prnt.sc/rMt0WORFqEpe 
+
