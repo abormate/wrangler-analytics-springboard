e60eae18c2bfebcdfd258e6cda95885e23a2c73a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-07 09:00:22 +0800

    Training - CodeWriting - Python for Analytics - Logical Operators

---
+++ fundamentals/python-programming-for-analytics/if-else-multiple-conditions/3-of-9_combination-logical-operators.py
@@ -0,0 +1,16 @@
+# ---------------------------------- #
+#
+# Combining Logical Operators with Python
+#
+# ---------------------------------- #
+
+# Learn 
+"""
+In the previous exercise, we computed the average rating of the apps with a genre that is either 
+"Social Networking" or "Games." We can ask even more specific questions, like these:
+
+-- What is the average rating of free apps with a genre that is either "Social Networking" or "Games"?
+-- What is the average rating of not free apps with a genre that is either "Social Networking" or "Games"?
+
+"""
+
