121c55f11f0fc735382266e871754034e2b556ab
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-08 11:19:20 +0800

    Training 05 - CodeWrite - Python for Analytics - Comparison App

--- fundamentals/python-programming-for-analytics/if-else-multiple-conditions/5-of-9_comparison-op-apps.py
+++ fundamentals/python-programming-for-analytics/if-else-multiple-conditions/5-of-9_comparison-op-apps.py
@@ -59,3 +59,16 @@ We can also use another approach to answer the question: we initialize a variabl
 and then increment that variable by 1 each time we find a rating of 4.0 or greater:
 
 """
+
+n_of_apps = 0
+
+for row in apps_data[1:]:
+    rating = float(row[7])
+    if rating >= 4.0:
+        n_of_apps = n_of_apps + 1
+
+print(n_of_apps)
+
+# Output
+4781
+
