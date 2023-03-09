5979b1096b6d056ca3d8b15f4a5f8c32d25c404a
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-09 14:18:56 +0800

    Training 07 - CodeWrite - Python for Analytics - Elif Clause

--- fundamentals/python-programming-for-analytics/if-else-multiple-conditions/7-of-9_the-elif-clause.py
+++ fundamentals/python-programming-for-analytics/if-else-multiple-conditions/7-of-9_the-elif-clause.py
@@ -20,5 +20,24 @@ price ≥ 50	        very expensive
 
 """
 
+# Using what we know, we can only do the transformations above using a combination of if statements. 
+# This is what the process looks like on the small dataset below:
+
+apps_data = [['Facebook', 0.0], ['Notion', 14.99], ['Astropad Standard', 29.99], 
+             ['NAVIGON Europe', 74.99]]
+
+for app in apps_data:
+    price = app[1]
+
+    if price == 0.0:
+        app.append('free')
+    if price > 0.0 and price < 20:
+        app.append('affordable')
+    if price >= 20 and price < 50:
+        app.append('expensive')
+    if price >= 50:
+        app.append('very expensive')
+
+print(apps_data)
 
 
