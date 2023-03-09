33530b76bfd0b5f23b3f934d33e3cab44a221059
Abraham Immanuel Bormate <53473761+abormate@users.noreply.github.com>
2023-03-09 14:17:54 +0800

    Training 07 - CodeWrite - Python for Analytics - Elif Statements

--- fundamentals/python-programming-for-analytics/if-else-multiple-conditions/7-of-9_the-elif-clause.py
+++ fundamentals/python-programming-for-analytics/if-else-multiple-conditions/7-of-9_the-elif-clause.py
@@ -5,3 +5,20 @@
 # ---------------------------------- #
 
 # Learn
+
+"""
+Let's do some more specific labeling than just "free" and "not free." We want to label 
+the apps using this convention
+
+
+price	            label
+------              ----------------
+price == 0	        free
+0 < price < 20	    affordable
+20 ≤ price < 50	    expensive
+price ≥ 50	        very expensive
+
+"""
+
+
+
